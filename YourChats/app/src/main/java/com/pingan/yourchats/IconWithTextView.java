6bb1adb7dd638388b1b3959b1f6b27b262e28f52
Ron <liupeng264@pingan.com.cn>
2016-11-21 18:34:55 +0800

    初始化

---
+++ YourChats/app/src/main/java/com/pingan/yourchats/IconWithTextView.java
@@ -0,0 +1,24 @@
+package com.pingan.yourchats;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Author：liupeng on 2016/11/21 16:46
+ * Address：liupeng264@pingan.com.cn
+ */
+public class IconWithTextView extends View {
+    public IconWithTextView(Context context) {
+        this(context,null);
+    }
+
+    public IconWithTextView(Context context, AttributeSet attrs) {
+        this(context, attrs,0);
+    }
+
+    public IconWithTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+    }
+}
